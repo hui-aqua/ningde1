9241f817fe5affcadf53ebead087ee8326f06601
Hui Cheng <2919784@uis.no>
2022-09-14 00:56:16 +0200

    added ningde case tried pyvista to show data

--- testCase/pyvista_case.py
+++ testCase/pyvista_case.py
@@ -1,5 +1,43 @@
 import pyvista as pv
 import numpy as np
+
+
+nodes = [
+    [0.0, 0.0, 0.0],
+    [0.0, 1.0, 0.0],
+    [4.0, 3.0, 0.0],
+    [4.0, 0.0, 0.0],
+    [0.0, 1.0, 2.0],
+    [4.0, 1.0, 2.0],
+    [4.0, 3.0, 2.0],
+]
+
+
+edges = [[0, 4],
+         [1, 4],
+         [3, 4],
+         [5, 4],
+         [6, 4],
+         [3, 5],
+         [2, 5],
+         [5, 6],
+         [2, 6]]
+widths=[float(i)/100.0 for i in range(9)]
+
+# We must "pad" the edges to indicate to vtk how many points per edge
+edges_w_padding=[[2]+item for item in edges]
+
+#mesh = pv.PolyData(nodes, edges_w_padding)
+#mesh.plot(
+#    scalars=colors,
+#    render_lines_as_tubes=True,
+#    style='wireframe',
+#    line_width=widths,
+#    cmap='jet',
+#    show_scalar_bar=False,
+#    background='w',
+#)
+
 xyz=np.array([[0,0,0],
 			  [1,0,0],
 			  [0,1,0],
@@ -16,6 +54,8 @@ p = pv.Plotter()
 p.add_mesh(pv.Arrow([1,1,1],[0,1,0]),  show_edges=True)
 #p.add_mesh(arrow([1,1,1],[0,0,1]), color="tan", show_edges=True)
 p.add_mesh(arrow, color="tan", show_edges=True)
+for item in edges:
+    p.add_mesh(pv.Tube(nodes[item[0]],nodes[item[1]],radius=widths[item[0]]))
 
 
 
