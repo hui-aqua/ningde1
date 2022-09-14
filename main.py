09203776fbd816200885539bedf85b95e338f8fc
Hui Cheng <chenghui62000@gmail.com>
2022-09-14 17:31:32 +0200

    clean home and format the ningde geomaker

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pyvista as pv
 import src.geoMaker.ningde as geo
 import src.visualization.saveVtk as sv
+
 # for circular cage
 # import src.case.circularCage as case
 # case.main()
