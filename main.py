c5cea1d30274392d43f8d0b072449c9b8f5522ef
Hui Cheng <chenghui62000@gmail.com>
2022-09-19 07:40:51 +0200

    save for today work, add the workflow for simulation. The line element need to be revised.

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ num_bodyPoint=31
 num_seg=50
 
 m_body=4980760 #[kg]
-l1=l.lines(geo.l1,10e9,0.5)
-l2=l.lines(geo.l2,10e9,1.0)
-l3=l.lines(geo.l3,10e9,1.5)
-l5=l.lines(geo.l5,10e9,2.5)
-lm=l.lines(geo.lm,10e9,0.01)
+# l1=l.lines(geo.l1,10e9,0.5)
+# l2=l.lines(geo.l2,10e9,1.0)
+# l3=l.lines(geo.l3,10e9,1.5)
+# l5=l.lines(geo.l5,10e9,2.5)
+# lm=l.lines(geo.lm,10e9,0.01)
 
 
 
@@ -36,10 +36,34 @@ lm=l.lines(geo.lm,10e9,0.01)
 # 
 fixed_point=[num_bodyPoint+num_seg*i for i in range(8)]
 
-
-
-
-
 xyz=np.array(nodes)
 dxyz=np.zeros_like(xyz)
 gravity=np.array([0,0,-9.81])
+
+
+run_time = 10  # unit [s]
+dt = 2e-2    # unit [s]
+
+# forward Euler (explicit)
+for i in range(int(run_time/dt)):       
+       
+    # gravity force
+    dxyz += dt*gravity
+    
+    # boundary condition
+    dxyz[fixed_point] *= 0.0  # velocity restriction
+    dxyz[xyz[:,2]<-22.0]*=np.array([1.0,1.0,0.0]) 
+    # velocity[fixed_point] *= np.array([1.0,1.0,0.0])  # fixed on xy plane
+    xyz += dt*dxyz
+    # print(dxyz)
+    # print(position0)
+    nodes = xyz.tolist()
+    # print(nodes)
+    if i % 5 == 0:
+        # sv.write_vtk('initial',point=nodes,line=line,face=face)
+        sv.write_vtk("ami2/"+"fa"+str(i),point=nodes,face=face)
+        sv.write_line_vtk("ami2/"+"l1"+str(i),point=nodes,line=geo.l1)
+        sv.write_line_vtk("ami2/"+"l2"+str(i),point=nodes,line=geo.l2)
+        sv.write_line_vtk("ami2/"+"l3"+str(i),point=nodes,line=geo.l3)
+        sv.write_line_vtk("ami2/"+"l5"+str(i),point=nodes,line=geo.l5)
+        sv.write_line_vtk("ami2/"+"lm"+str(i),point=nodes,line=geo.lm)
--- src/element/line.py
+++ src/element/line.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 row_water=1000.0
+row_air=1.001
 kinematic_viscosity = 1.004e-6  # when the water temperature is 20 degree.
 dynamic_viscosity = 1.002e-3  # when the water temperature is 20 degree.
 
@@ -51,9 +52,11 @@ class lines:
         force_on_point[self.np_index[:,0]]+=force1
         force_on_point[self.np_index[:,1]]+=force2
         return force_on_point
-    def calculate_extenal_force(self,node_position,u):
-        
     
+    
+    def calculate_extenal_force(self,node_position,u): 
+        # calculate the buoyancy and drag force (based on Morison equation).
+        pass
     
     
     def map_hydrodynamic_force(self,forces):
@@ -88,11 +91,21 @@ class lines:
             exit()
         return drag_normal, drag_tangent
 
+    def __cal_aw_ratio(self,):
+        # calculate the ratio between air and water
+        # consider as slender bar
+        pass
+    
+    
     def __calculate_buoyancy_force(self,node_position, elevation):
-        
-        num_line = len(self.line_elements)
+                
         # force on line element, initial as zeros
-        force_on_element = np.zeros((num_line, 3),dtype=float)
+        force_on_element = np.zeros((self.number_of_line, 3),dtype=float)
+        
+        p1_z=elevation[self.np_index[:,0]]-node_position[self.np_index[:,0]][:,2]
+        p2_z=elevation[self.np_index[:,1]]-node_position[self.np_index[:,1]][:,2]
+        
+        
         for index, element in enumerate(self.line_elements):
             p1 = node_position[int(element[0])]
             p2 = node_position[int(element[1])]
@@ -100,12 +113,12 @@ class lines:
             list_z = []
             for each in element:
                 list_z.append(float(elevation[each] - node_position[each][2]))
-            ratio = self.cal_aw_ratio(list_z)
+            ratio = self.__cal_aw_ratio(list_z)
             row = row_water*ratio+row_air*(1-ratio)
 
             element_length = np.linalg.norm(p1-p2)
-            element_volume = 0.25*pi*pow(self.dwh, 2)*element_length           
-            force_on_element[index] = [0.0, 0.0, element_volume*gravity*row]
+            element_volume = 0.25*np.pi*pow(self.dwh, 2)*element_length           
+            force_on_element[index] = [0.0, 0.0, element_volume*9.81*row]
 
         self.hydro_static_forces = np.array(force_on_element)
         return np.array(force_on_element)
@@ -129,3 +142,32 @@ class lines:
         fn=0.5*cn*row_water*self.dw0*self.line_length*un*np.linalg.norm(un,axis=1)
 
         return ft+fn
+    
+    
+    
+# class pipe(line):
+    
+#     """
+#     For Morison hydrodynamic models, the forces on netting are calculated based on individual pipe.
+#     The twines are taken as cylindrical elements. In practice, the force is usually decomposed into two components:
+#     normal drag force F_n and tangential drag force F_t (Cheng et al., 2020)
+#     """
+#     def __init__(self,index:list,stiffness:float,dw0:float,thickness:float):
+#         self.k=stiffness # unit [N/m] a.k.a -> K
+#         self.index=index
+#         self.np_index=np.array(index)
+#         self.number_of_point=len(np.unique(np.array(self.index)))
+#         self.number_of_line=len(index)
+#         self.initial_line_length=0
+#         self.dw0=dw0
+#         self.t=thickness
+            
+#     def __cal_aw_ratio(self,):
+#         # calculate the ratio between air and water
+#         # consider as thick pipe
+#         pass
+    
+
+
+if __name__ == "__main__":
+    pass
